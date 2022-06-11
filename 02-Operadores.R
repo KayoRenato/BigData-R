48b68dfedfdbde81dc2f582099401edc68bca142
Kayo.io <kayorenatocontato@gmail.com>
2022-06-11 13:47:46 -0300

    Update: Add R Script about Logical Operation

---
+++ 02-Operadores.R
@@ -0,0 +1,56 @@
+#----Operacoes basica
+#adicao
+4 + 3
+
+#subtracao
+6 - 2
+
+#multiplicacao
+3 * 45
+
+#divisao
+45 / 4
+
+#potencia
+2^8
+2**8
+
+#modulo
+34 %% 7
+
+#----Operadores Relacionais
+
+#atribuicao de variaveis
+x = 7
+y = 5
+
+#Operadores
+x > 8
+
+x < 8
+
+x <= 7
+
+x >= 8
+
+x == 8
+
+x != 8
+
+#---Operadores logicos
+
+#and
+(x==8) & (x==6)
+
+(x==7) & (x==5)
+
+(x==8) & (x==5)
+
+#or
+(x==8) | (x==5)
+
+(x==8) | (x>=5)
+
+#not
+x > 8
+print(!x > 8)
