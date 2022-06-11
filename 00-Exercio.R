bf62e65d47b408d21022cc13d5478fab26fe3698
Kayo.io <kayorenatocontato@gmail.com>
2022-06-11 20:53:49 -0300

    Update: Add exercise of R Script

---
+++ 00-Exercio.R
@@ -0,0 +1,52 @@
+# Exercício 1 - Crie um vetor com 30 números inteiros
+vec1 <- c(1:30)
+vec1
+
+# Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
+mtx1 <- matrix(c(1:16), nrow = 4, ncol = 4)
+mtx1
+
+# Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
+list1 <- list(c(vec1, mtx1))
+list1
+
+# Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
+# http://data.princeton.edu/wws509/datasets/effort.dat
+
+data1 <-data.frame(read.table(url('https://data.princeton.edu/wws509/datasets/effort.dat')))
+data1
+
+
+# Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
+# c("config", "esfc", "chang")
+
+names(data1) <- c("config", "esfc", "chang")
+data1
+
+# Exercício 6 - Imprima na tela o dataframe iris, verifique quantas dimensões existem no dataframe iris e imprima um resumo do dataset
+View(iris)
+dim(iris)
+str(iris)
+
+# Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+head(iris)
+plot(iris$Sepal.Length, iris$Sepal.Width)
+
+# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+iris_len_7 <- subset(iris, iris$Sepal.Length > 7)
+View(iris_len_7)
+
+# Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), divida o dataframe em um subset de 15 linhas
+# Dica 1: Você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+library(dplyr)
+iris_slice <- slice_sample(iris, n=15)
+View(iris_slice)
+
+# Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+RSiteSearch('filter')
+iris_slice_len_6 <- filter(iris_slice, iris_slice$Sepal.Length>6)
+iris_slice_len_6
+
